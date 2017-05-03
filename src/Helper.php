d9fd793657d3a291a3437a389166b418f09deee3
Sébastien CAPARROS <caparros@novius.fr>
2017-05-03 13:50:49 +0200

    Added a Helper class

---
+++ src/Helper.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace SebC\AdditionalPhpCsFixers;
+
+class Helper
+{
+    /**
+     * Helper to easily declare custom fixers to PhpCsFixer
+     * `$phpCsFixerConfig->registerCustomFixers(Helper::getCustomFixers())`
+     */
+    public static function getCustomFixers()
+    {
+        return [
+            new SebC\AdditionalPhpCsFixers\DisallowUnaliasedClasses(),
+        ];
+    }
+}
